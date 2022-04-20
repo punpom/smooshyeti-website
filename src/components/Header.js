c7b62453bb53d2a1abe168540d4d826c288c0175
Lucas <loukaka41@gmail.com>
2022-04-20 14:10:40 +0200

    update

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+function Header() {
+    return (
+      <div className="App">
+        <nav>
+          <div className="navbar-left"></div>
+          <div className="navbar-right">
+          <ul>
+            <li><a className="links-nav" href='#'>MINT</a></li>
+            <li><a className="links-nav" href='#'>ROADMAP</a></li>
+            <li><a className="links-nav" href='#'>DISCORD</a></li>
+            <li><a className="links-nav" href='#'>WHITEPAPER</a></li>
+            <li><a className="links-nav" href='#'>TEAM</a></li>
+            <li><a className="links-nav" href='#'>FAQ</a></li>
+          </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+  
+  export default Header;
